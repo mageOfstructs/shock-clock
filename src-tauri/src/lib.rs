48ef3e121e9079b9d1b50044f6c63e886ee04f41
ThronKatze0 <vincentius.winkler@gmail.com>
2024-12-18 15:49:35 +0100

    accessibility service spyware

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,7 @@
 mod watcher_state;
+use serde::{Deserialize, Serialize};
 use tauri::async_runtime::Mutex;
+use tauri::Listener;
 
 mod ble;
 
@@ -9,11 +11,26 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[derive(Deserialize, Serialize)]
+struct AccessibilityEvent {
+    message: String,
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            println!("starting to listen lol");
+            app.listen_any("accessibilityEvent", |event| {
+                if let Ok(payload) = serde_json::from_str::<AccessibilityEvent>(&event.payload()) {
+                    println!("New event {}", payload.message);
+                }
+            });
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_blec::init())
+        .plugin(tauri_plugin_accessibility::init())
         .manage(Mutex::new(Vec::<shock_clock_utils::Block>::new()))
         .manage(Mutex::new(Option::<String>::None))
         .invoke_handler(tauri::generate_handler![
