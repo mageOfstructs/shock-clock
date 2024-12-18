2782d326e5a86542c09b8b22ebf02eb5bd13c312
mageOfStructs <jason.puschnig@gmx.at>
2024-12-18 10:57:20 +0100

    alter Freund der derive-Makros

--- shock-clock-utils/src/lib.rs
+++ shock-clock-utils/src/lib.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use uuid;
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Block {
     pub uuid: uuid::Uuid,
     pub name: String,
@@ -9,31 +9,31 @@ pub struct Block {
     pub block_type: BlockType,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum BlockType {
     App(AppBlockData),
     Website(WebsiteBlockData),
     Keyword(KeywordBlockData),
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum ShockStrength {
     Normal,
     Hard,
     Ultra,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct AppBlockData {
     pub package_name: String,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct WebsiteBlockData {
     pub url: String,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct KeywordBlockData {
     pub name: String,
 }
