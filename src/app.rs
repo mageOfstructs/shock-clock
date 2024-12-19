10f9e725af1074ffd3144e3533e066ee434ceaf9
ThronKatze0 <vincentius.winkler@gmail.com>
2024-12-19 18:59:23 +0100

    wasm stuff2

--- src/app.rs
+++ src/app.rs
@@ -13,6 +13,19 @@ use leptos_icons::*;
 use leptos_mview::mview;
 use shock_clock_ui::components::{Games, Home, Watcher};
 
+#[wasm_bindgen]
+extern "C" {
+    // invoke without arguments
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"], js_name = invoke)]
+    pub async fn invoke_without_args(cmd: &str) -> JsValue;
+
+    // invoke with arguments (default)
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
+    pub async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+
+    // They need to have different names!
+}
+
 #[derive(Clone, PartialEq, Copy)]
 enum SelectedRoute {
     Watcher,
