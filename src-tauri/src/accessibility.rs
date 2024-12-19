b42f333390e06ab795b8caa2baac84d184a374f9
ThronKatze0 <vincentius.winkler@gmail.com>
2024-12-19 20:10:31 +0100

    thread stuff

--- src-tauri/src/accessibility.rs
+++ src-tauri/src/accessibility.rs
@@ -11,6 +11,7 @@ pub async fn init_accessibility(
     state: State<'_, Mutex<Vec<shock_clock_utils::Block>>>,
 ) -> Result<(), ()> {
     async_runtime::block_on(async {
+        println!("init accessibility service");
         let mut lock = state.lock().await;
 
         loop {
@@ -20,12 +21,10 @@ pub async fn init_accessibility(
                     "{}\n{}\n{}\n\n",
                     event.package, event.event_type, event.text
                 );
-                async_runtime::block_on(async {
-                    println!("thread spawned");
-                    println!("{}", lock.len());
+                println!("thread spawned");
+                println!("{}", lock.len());
 
-                    check_for_block(event, lock);
-                });
+                check_for_block(event, &lock).await;
             }
         }
     });
@@ -34,11 +33,31 @@ pub async fn init_accessibility(
 
 async fn check_for_block(
     accessibility_event: AccessibilityEvent,
-    blocks: MutexGuard<'_, Vec<Block>>,
+    blocks: &MutexGuard<'_, Vec<Block>>,
 ) {
-    blocks.iter().for_each(|block| {
-        match block.block_type {
-            BlockType::Keyword => 
+    for block in blocks.iter() {
+        let blocked = match &block.block_type {
+            BlockType::Keyword => process_keyword(&accessibility_event, block.name.clone()).await,
+            BlockType::Website(data) => {
+                process_keyword(&accessibility_event, data.url.clone()).await
+            }
+            BlockType::App(data) => {
+                process_app(&accessibility_event, data.package_name.clone()).await
+            }
+        };
+        if blocked {
+            println!("Block: {:?} {:?}", block.block_type, block.shock_strength);
+            break;
         }
-    });
+    }
+}
+
+async fn process_keyword(accessibility_event: &AccessibilityEvent, keyword: String) -> bool {
+    // accessibility_event.text.contains(&keyword)
+    true
+}
+
+async fn process_app(accessibility_event: &AccessibilityEvent, package: String) -> bool {
+    if accessibility_event.package != "com.shock_clock.app" {}
+    true
 }
